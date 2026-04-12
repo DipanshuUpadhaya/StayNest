94c373f418775eb4a96a4bd8d81dda78c4405fea
dipanshuupashyay123 <dipanshuupadhyay45@gmail.com>
2026-04-13 02:02:38 +0530

    final fix cloudinary upload

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,20 +1,24 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+// Cloudinary config
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
+// Storage config
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'wanderlust_DEV',
-    allowed_formats:["png","jpg","jpeg"],
-  },
+    cloudinary: cloudinary,
+    params: {
+        folder: "wanderlust_DEV",
+        allowed_formats: ["png", "jpg", "jpeg"],
+        resource_type: "image",   //  VERY IMPORTANT
+    },
 });
 
-module.exports={
+module.exports = {
     cloudinary,
     storage,
 };
